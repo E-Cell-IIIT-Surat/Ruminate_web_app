788ffeb66c4103522576c19f84228d151769f556
shekharhimanshu108 <shekharhimanshu1511@gmail.com>
2026-02-21 22:28:36 +0530

    Update EventHighlights.tsx

--- src/app/home/EventHighlights.tsx
+++ src/app/home/EventHighlights.tsx
@@ -2,16 +2,18 @@
 import styles from "./home.module.css";
 
 const events = [
-  { id: 1, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/AGC_20241008_145744337.jpg", title: "Event 1" },
-  { id: 2, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%200J3A6821.jpg", title: "Event 2" },
-  { id: 3, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%200J3A8218.JPG", title: "Event 3" },
-  { id: 4, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%200J3A8225.JPG", title: "Event 4" },
-  { id: 5, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%208A9A4583.JPG", title: "Event 5" },
-  { id: 6, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%208A9A4791.JPG", title: "Event 6" },
-  { id: 7, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%208A9A4879.JPG", title: "Event 7" },
-  { id: 8, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Copy%20of%20IMG_5071.JPG", title: "Event 8" },
-  { id: 9, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/E-summit%28Speaker-session%29.JPG", title: "Event 9" },
-  { id: 10, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/ICCC_Visit.JPG", title: "Event 10" }
+  { id: 1, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2012.png", title: "Event 1" },
+  { id: 2, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2013.png", title: "Event 2" },
+  { id: 3, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2014.png", title: "Event 3" },
+  { id: 4, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2015.png", title: "Event 4" },
+  { id: 5, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2020.png", title: "Event 5" },
+  { id: 6, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2021.png", title: "Event 6" },
+  { id: 7, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2022.png", title: "Event 7" },
+  { id: 8, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2023.png", title: "Event 8" },
+  { id: 9, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2024.png", title: "Event 9" },
+  { id: 10, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2025.png", title: "Event 10" },
+  { id: 11, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2026.png", title: "Event 11" },
+  { id: 12, image: "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/Event%20Highlights/Frame%2027.png", title: "Event 12" }
 ];
 
 
