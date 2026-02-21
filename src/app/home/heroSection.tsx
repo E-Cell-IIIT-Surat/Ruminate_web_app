ca986131842931cb3e886e56bf79c8ea8ba40e13
shekharhimanshu108 <shekharhimanshu1511@gmail.com>
2026-02-21 13:04:56 +0530

    Update heroSection.tsx

--- src/app/home/heroSection.tsx
+++ src/app/home/heroSection.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 import styles from "./home.module.css";
 
 const heroSlides = [
-  "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/amulvisit.png",
-  "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/iprsession.png",
+  "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/voteofthanks.png",
   "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/IPLAUCTION.png",
   "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/alpeshsirsession.png",
   "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/certificatedistribution.png",
   "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/corporatecrime.png",
   "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/youthparliament.png",
+  "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/amulvisit.png",
+  "https://pub-d9e37e07152c4e608d951985e3cf2832.r2.dev/homepageslider/iprsession.png",
 ];
 
 const heroBoxes = [
